77aae2c5153d217992dd932ed91f0cbd67be3a68
root <root@bj-ops3.(none)>
2018-04-19 15:42:37 +0800

    +e6ec16d7fc8e29e4abfa63e47936193fa5e7cb65

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
-  <div id="app">
-        <router-view></router-view>
-        <app-footer  slot="bottom"></app-footer>
-
+  <div id="app" height="100%">
+      
+      <router-view></router-view>
+      <app-footer  slot="bottom"></app-footer>
+        
   </div>
 </template>
 
@@ -10,7 +11,10 @@
 @import '~vux/src/styles/reset';
 
 html, body {
+  width:100%;
+  height:100%;
   -webkit-overflow-scrolling: touch!important;
+  overflow-x: hidden;
   background-color:#ffffff;
 }
 #app{
