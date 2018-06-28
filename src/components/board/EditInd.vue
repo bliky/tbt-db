503c060910e2905b9b5526e32967641e93fbd723
opsat <opsat@corp.to8to.com>
2018-06-28 10:45:35 +0800

    +4561fb1dfda0a5c5efaf5ed3accc4154eb99078d

--- src/components/board/EditInd.vue
+++ src/components/board/EditInd.vue
@@ -2,29 +2,40 @@
 <div class="editInd" style="height:100%;margin: 15px 0px 0px 0px;padding-bottom:80px;-webkit-overflow-scrolling:touch;">
    <div style="margin-left:20px;">
     <div style="font-family:PingFangSC-Medium;font-size: 20px;color: #333333;"> 编辑指标</div>
-    <div style="font-family:PingFangSC-Regular;font-size: 12px;color: #C1C1C1;">点击左边图标“眼睛”显示或隐藏指标</div>
+    <div style="font-family:PingFangSC-Regular;font-size: 12px;color: #C1C1C1;">点击左边图标“眼睛”显示或隐藏指标,长按右边图标拖拽排序</div>
    </div>  
-    
+
    <!-- <div  v-for="(item,index) in listData" :key="index" class="showItem" style="margin-top:20px;margin-bottom:50px;padding-left:20px;font-family: PingFangSC-Regular;font-size: 17px;color: #333333;"> -->
      <div style="margin-top:20px;padding-bottom:80px;padding-left:20px;font-family: PingFangSC-Regular;font-size: 17px;color: #333333;">
       <hr color="#e3e3e3" size="1px">
-      <checker v-model="hiddenItem" v-for="(item, index) in showList" :key="index"
-               type="checkbox">
-        <div :class="{'showInd':item.is_show == 'true','hiddenInd':item.is_show == 'false'}" style="display: flex;align-items: center;font-family: PingFangSC-Regular;font-size: 17px;">
-           <label class="showItem" @click="onItemClickHidden(item,item.ind_id,item.dim_ind_name,index)" padding="550px">
-              <img v-if="item.is_show == 'true'" width="22px" height="22px" style="margin-right:20px" align="center"  
-                src="../../assets/image/icon_eye_open@2x.png">
-              <img v-if="item.is_show == 'false'" width="22px" height="22px" style="margin-right:20px" align="center" 
-                  src="../../assets/image/icon_eye_close@2x.png">
-            </label>
-          <checker-item :value="item" class="showItem">
-              {{item.dim_ind_name}}
-          </checker-item>
-          
-        </div>
-       <hr color="#eeeeee" size="1px">
-      </checker>
-      <div>
+       <draggable :list="dataList" 
+            :move="onRemove" 
+            :options="{animation: 150,handle:'.dargImg'}">
+           <transition-group name="list-complete">
+            <checker v-model="hiddenItem" v-for="(item, index) in dataList" :key="index" v-if="item.dim_ind_name != ''"
+                    type="checkbox">
+              <div :class="{'showInd':item.is_show == 'true','hiddenInd':item.is_show == 'false'}" style="display: flex;align-items: center;font-family: PingFangSC-Regular;font-size: 17px;">
+                <label :hidden="curDraftingName == item.dim_ind_name" class="showItem" @click="onItemClickHidden(item,item.ind_id,item.dim_ind_name,index)" >
+                    <img v-if="item.is_show == 'true'" width="22px" height="22px" style="margin-right:20px" align="center"  
+                      src="../../assets/image/icon_eye_open@2x.png">
+                    <img v-if="item.is_show == 'false'" width="22px" height="22px" style="margin-right:20px" align="center" 
+                        src="../../assets/image/icon_eye_close@2x.png">
+                  </label>
+                <checker-item :value="item"  class="showItem" :hidden="curDraftingName == item.dim_ind_name">
+                    {{item.dim_ind_name}}
+                </checker-item>
+                <label :hidden="curDraftingName == item.dim_ind_name" class="dargImg showItem"  style="width:20%;position:static;text-align:right;float:right;clear: both; display: block;margin:auto;margin-right:10px; ">
+                       <!-- <span>{{item.order_num}}</span> -->
+                   <img v-if="item.is_show == 'true'" width="23px" height="23px" style="padding-right:10px;"
+                        align="center"  
+                        src="../../assets/image/icon_handle@2x.png"> 
+                </label>
+              </div>
+            <hr color="#eeeeee" size="1px">
+            </checker>
+          </transition-group>  
+       </draggable> 
+      <!-- <div>
         <checker v-model="showItem" v-for="(item, index) in hiddenList" :key="index" type="checkbox">
           <div :class="{'showInd':item.is_show == 'true','hiddenInd':item.is_show == 'false'}" style="display: flex;align-items: center;font-family: PingFangSC-Regular;font-size: 17px;">
              <label class="hiddenItem" @click="onItemClickShow(item,item.ind_id,item.dim_ind_name,index)" width="40px" height="40px">
@@ -40,7 +51,7 @@
           </div>
         <hr color="#eeeeee" size="1px">
         </checker>
-      </div>
+      </div> -->
   </div>
 
   <div class="indBtn">
@@ -60,10 +71,29 @@
           :position="position">
       </toast>
   </div>
+
+   
 </div>
 </template>
 
 <style  lang="less" scoped>
+.sortable-ghost {
+  opacity: 0;
+  // background-color: #F4E2C9;
+}
+// .sortable-drag{
+//  background:#bebebe;
+// }
+// .sortable-chosen{
+//  background:red;
+// }
+// .sortable-fallback{
+//  background:black;
+// }
+.dargImg{
+  cursor:move;
+}
+
 .showInd{
 color: #333333;
 }
@@ -105,6 +135,7 @@ color: #333333;
 }
 </style>
 <script>
+import draggable from 'vuedraggable'
 import Cookie from "js-cookie";
 import { formatDate } from "../../utils/date.js";
 import {
@@ -126,10 +157,12 @@ import {
 import { setTimeout } from "timers";
 
 export default {
+  
   directives: {
     TransferDom
   },
   components: {
+    draggable,
     Toast,
     formatDate,
     Tabbar,
@@ -144,29 +177,86 @@ export default {
   },
   data() {
     return {
+      drag: false,
+      drop: false,
       uid: "",
       uname: "",
       save: false,
       isSelect: false,
       position: "middle",
       token: "",
-      //listData: [{
-      showList: [], //获取的显示数据
+      curDraftingName: "",//当前拖拽名称
+      curDraftingList: {},//当前拖拽数据
+      //listData: [{//没有用到
+      dataList: [],
+      showList: [], //获取的显示数据，暂时没有用到
       showListBack: [], //显示数据初始化备份
-      hiddenList: [], //获取的隐藏数据
+      hiddenList: [], //获取的隐藏数据，暂时没有用到
       hiddenListBack: [], //隐藏数据初始化备份
       showItem: [], //显示数组
       hiddenItem: [] //隐藏数组
     };
   },
   mounted: function() {
+    
+    Array.prototype.insert = function (index, item) {
+         this.splice(index, 0, item);
+    }
     this.uid = Cookie.get("t8t-it-uid");
     this.uname = Cookie.get("t8t-oa-username");
     this.token = Cookie.get("t8t-it-token");
 
     this.getList();
   },
+  
   methods: {
+    onAdd: function (evt){ //拖拽时候添加有新的节点的时候发生该事件
+   console.log('onAdd.foo:', [evt.item, evt.from]); 
+  },
+  onUpdate: function (evt){ //拖拽更新节点位置发生该事件
+   console.log('onUpdate.foo:', [evt.item, evt.from]); 
+  },
+  onRemove: function (evt){ //删除拖拽节点的时候促发该事件
+   console.log('onRemove.foo:', [evt.item, evt.from]); 
+  },
+  onStart:function(evt){ //开始拖拽出发该函数
+  this.curDraftingName = null
+  const startList = [evt.item, evt.from]
+   console.log('onStart.foo:', startList);
+   console.log("innerText=========")
+   console.log(startList[0].innerText)
+   this.curDraftingName = startList[0].innerText
+  },
+  onSort:function(evt){ //发生排序发生该事件
+   console.log('onSort.foo:', [evt.item, evt.from]);
+  },
+  onEnd: function(evt){ //拖拽完毕之后发生该事件
+   console.log('onEnd.foo:', [evt.item, evt.from]); 
+  },
+    getdata (evt) {
+      // console.log("evt==========")
+      // console.log(evt)
+      // let curElement = evt
+      // this.curDraftingName = curElement.relatedContext.element.dim_ind_name
+      // this.curDraftingList = curElement.relatedContext.element
+      // console.log("开始")
+      // console.log(curElement)
+      // console.log(this.curDraftingName)
+      // console.log(this.curDraftingList)
+      },
+      datadragEnd (evt) {
+        // let oindex = evt.oldIndex 
+        // let nindex = evt.newIndex
+        // console.log('拖动前的索引 :' + evt.oldIndex)
+        // console.log('拖动后的索引 :' + evt.newIndex)
+        // this.curDraftingName = this.showList[nindex].dim_ind_name
+        // console.log(this.curDraftingName)
+        // console.log("this.dataList======")
+        // console.log(this.dataList)
+        // let index = evt.newIndex
+        // this.showListBack.insert(index,this.curDraftingList)
+        // console.log(this.showListBack)
+      },
     onItemClickHidden(item, ind_id, dim_ind_name, index) {
       let date = new Date();
       date = formatDate(date, "yyyy-MM-dd hh:mm:ss");
@@ -230,20 +320,39 @@ export default {
       }
     },
     clickDetermine() {
-      for (let i = 0; i < this.showListBack.length; i++) {
-        this.showListBack[i].order_num = i + 1;
+     
+      for(let arr of this.dataList){
+        if(arr.is_show == 'true'){
+         this.showList.push(arr)
+        }
+        if(arr.is_show == 'false'){
+          this.hiddenList.push(arr)
+        }
       }
-      for (let i = 0; i < this.hiddenListBack.length; i++) {
-        this.hiddenListBack[i].order_num = i + 1;
+      // for(let i = 0; i < this.dataList.length; i++){
+      //   this.dataList[i].is_show = 'false'
+      //   this.dataList.splice(i,1)
+      // }
+      // console.log("保存dataList====")
+      // console.log(this.dataList)
+      // this.showListBack = this.dataList
+      // console.log("this.showListBack===1=============")
+      // console.log(this.showListBack)
+      for (let i = 0; i < this.showList.length; i++) {
+        this.showList[i].order_num = i + 1;
       }
       
+      for (let i = 0; i < this.hiddenList.length; i++) {
+        this.hiddenList[i].order_num = i + 1;
+      }
+     
       this.$http
         .fetch("dsa/dataBoard/editInd", {
           uid: this.uid,
           dbuaToken: this.token,
           uname: this.uname,
-          showList: this.showListBack,
-          hiddenList: this.hiddenListBack
+          showList: this.showList,
+          hiddenList: this.hiddenList
         })
         .then(
           response => {
@@ -276,38 +385,44 @@ export default {
           response => {
             if (response.data.status == 200) {
               console.log("成功了");
-              this.showList = [];
-              this.hiddenList = [];
-              this.showList = response.data.result.showList;
-              this.hiddenList = response.data.result.hiddenList;
-              for (var i = this.showList.length - 1; i >= 0; i--) {
-                this.showList[i].dim_ind_name = this.showList[
-                  i
-                ].dim_ind_name.replace("汇总", "");
-              }
+              this.dataList = []
+              this.showList = []
+              this.hiddenList = []
+              this.showListBack = []
+              this.hiddenListBack = []
+       
+              this.dataList = response.data.result.dataList
 
-              for (var i = this.hiddenList.length - 1; i >= 0; i--) {
-                this.hiddenList[i].dim_ind_name = this.hiddenList[
+              // this.showList = response.data.result.showList
+              // this.hiddenList = response.data.result.hiddenList
+
+              this.showListBack = response.data.result.showList
+              this.hiddenListBack = response.data.result.hiddenList
+              for (var i = this.dataList.length - 1; i >= 0; i--) {
+                this.dataList[i].dim_ind_name = this.dataList[
                   i
                 ].dim_ind_name.replace("汇总", "");
               }
-            }
-          },
-          response => {}
-        );
-      this.$http
-        .fetch("dsa/dataBoard/getEditIndList", {
-          uid: this.uid,
-          dbuaToken: this.token,
-          uname: this.uname
-        })
-        .then(
-          response => {
-            if (response.data.status == 200) {
-              this.showListBack = [];
-              this.hiddenListBack = [];
-              this.showListBack = response.data.result.showList;
-              this.hiddenListBack = response.data.result.hiddenList;
+              
+              console.log("this.dataList===list=============")
+              console.log(this.dataList)
+              console.log("this.showListBack===list=============")
+              console.log(this.showListBack)
+              console.log("this.hiddenListBack===list=============")
+              console.log(this.hiddenListBack)
+
+              // for (var i = this.showList.length - 1; i >= 0; i--) {
+              //   this.showList[i].dim_ind_name = this.showList[
+              //     i
+              //   ].dim_ind_name.replace("汇总", "");
+              // }
+
+              // for (var i = this.hiddenList.length - 1; i >= 0; i--) {
+              //   this.hiddenList[i].dim_ind_name = this.hiddenList[
+              //     i
+              //   ].dim_ind_name.replace("汇总", "");
+              // }
+
               for (var i = this.showListBack.length - 1; i >= 0; i--) {
                 this.showListBack[i].dim_ind_name = this.showListBack[
                   i
@@ -323,6 +438,34 @@ export default {
           },
           response => {}
         );
+      // this.$http
+      //   .fetch("dsa/dataBoard/getEditIndList", {
+      //     uid: this.uid,
+      //     dbuaToken: this.token,
+      //     uname: this.uname
+      //   })
+      //   .then(
+      //     response => {
+      //       if (response.data.status == 200) {
+      //         this.showListBack = [];
+      //         this.hiddenListBack = [];
+      //         this.showListBack = response.data.result.showList;
+      //         this.hiddenListBack = response.data.result.hiddenList;
+      //         for (var i = this.showListBack.length - 1; i >= 0; i--) {
+      //           this.showListBack[i].dim_ind_name = this.showListBack[
+      //             i
+      //           ].dim_ind_name.replace("汇总", "");
+      //         }
+
+      //         for (var i = this.hiddenListBack.length - 1; i >= 0; i--) {
+      //           this.hiddenListBack[i].dim_ind_name = this.hiddenListBack[
+      //             i
+      //           ].dim_ind_name.replace("汇总", "");
+      //         }
+      //       }
+      //     },
+      //     response => {}
+      //   );
     }
   }
 };
