240ec4f4abf2f1572b147f8c6bb1df2e4fd99804
ken.li <ken.li@corp.to8to.com>
2019-07-18 13:59:41 +0800

    更改核心指标小数据点位数

--- src/components/branch/numFormater.js
+++ src/components/branch/numFormater.js
@@ -13,8 +13,9 @@ export default (val, data_type) => {
       if (fval >= 10000) {
         fval /= 10000;
         suffix = '万';
+        return filterNumber(fval, '0,0.00') + suffix;
       }
-      return filterNumber(fval, '0,0') + suffix;
+      return filterNumber(fval, '0,0');
     case 2:
       return filterNumber(fval*100, '0,0.00', '', '%') + suffix;
   }
